5e736fe9bbe9fc943cef1422970bf26a422269a2
yllikrasniqi4-design <ylli.krasniqi4@student.uni-pr.edu>
2026-01-02 21:16:06 +0100

    Update funksionet.cpp

--- funksionet.cpp
+++ funksionet.cpp
@@ -127,6 +127,43 @@ void numriiStudenteveDeshtues() {
 
     cout << "Studente deshtues: " << deshtues << endl;
 }
+void ndryshoNoten() {
+int index, nota;
+cout &lt;&lt; &quot;Shkruaj numrin e studentit: &quot;;
+cin &gt;&gt; index;
+
+if (index &lt; 1 || index &gt; total) {
+cout &lt;&lt; &quot;Index i gabuar.\n&quot;;
+return;
+}
+
+cout &lt;&lt; &quot;Shkruaj noten e re: &quot;;
+cin &gt;&gt; nota;
+
+if (nota &lt; 5 || nota &gt; 10) {
+cout &lt;&lt; &quot;Nota e pavlefshme.\n&quot;;
+return;
+}
+
+notatMatematike[index - 1] = nota;
+cout &lt;&lt; &quot;Nota u perditesua.\n&quot;;
+
+}
+
+void llogaritMesataren() {
+if (total == 0) {
+cout &lt;&lt; &quot;Nuk ka studente.\n&quot;;
+return;
+}
+
+int shuma = 0;
+for (int i = 0; i &lt; total; i++)
+shuma += notatMatematike[i];
+
+cout &lt;&lt; &quot;Mesatarja: &quot;
+&lt;&lt; (float)shuma / total &lt;&lt; endl;
+}
+
 void menu() {
     cout << "\n===== MENAXHIMI I NOTAVE =====\n";
     cout << "1. Regjistro student\n";
