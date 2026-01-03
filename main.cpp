c677d24bb2aa21ecc40937f7c341bda2e8876ffb
lirushi <l.malazogu@gmail.com>
2026-01-03 11:17:13 +0100

    Perditsova fajllin main per te zgjedhur funksionet

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "funksionet.h"
+
+using namespace std;
+
+int main() {
+    int zgjedhja;
+    do {
+        menu();
+        cin >> zgjedhja;
+
+        switch (zgjedhja) {
+        case 1:
+            shtoStudent();
+            break;
+        case 2:
+            shfaqStudentet();
+            break;
+        case 3:
+            kerkoStudent();
+            break;
+        case 4:
+            fshijStudent();
+            break;
+        case 5:
+            ndryshoNoten();
+            break;
+        case 6:
+            llogaritMesataren();
+            break;
+        case 7:
+            numriStudenteve();
+            break;
+        case 8:
+            numriStudenteveKalues();
+            break;
+        case 9:
+            numriStudenteveDeshtues();
+            break;
+        case 0:
+            cout << "Programi u mbyll me sukses.\n";
+            break;
+        default:
+            cout << "Zgjedhje e gabuar. Provo perseri.\n";
+        }
+
+    } while (zgjedhja != 0);
+
+    return 0;
+}
